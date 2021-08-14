be2b6b82cd11aa93bc87087a5d1caa97e6109513
Bradley Legg <48114146+computerScienceMan@users.noreply.github.com>
2021-08-13 22:27:32 -0400

    Add files via upload

---
+++ HW1_Improved/HW1_Website/JavaScript/script.js
@@ -0,0 +1,8 @@
+/*
+File: script.js
+Author: Bradley R. Legg, UMass Lowell Computer Science, bradley_legg@student.uml.edu
+a. Bradley R. Legg
+b. 8 August 2021
+c. The script.js tag is used as a placeholder file as JS is unnecessary here,
+   but typically would handle backend features of the website.
+*/
